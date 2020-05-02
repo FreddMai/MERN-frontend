78a6d0a12ab5e1dd86dedd2525ec1520966a8af2
Freddy M <fredtein@gmail.com>
2020-05-02 13:04:13 +0300

    First init - User and place components, styling and more

---
+++ src/user/pages/Users.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import UsersList from "../components/UsersList";
+
+const Users = () => {
+  const USERS = [
+    {
+      id: "u1",
+      name: "Fred",
+      image: "https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
+};
+
+export default Users;
