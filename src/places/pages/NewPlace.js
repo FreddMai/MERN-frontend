30cafe8fa5de4d67c5eafe8ee36492745bf60bc6
Freddy M <fredtein@gmail.com>
2020-06-20 18:15:02 +0300

    Adding auth page: switch mode, auth context

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -5,7 +5,7 @@ import Button from "../../shared/components/FormElements/Button";
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH,
-} from "../../shared/util/validators.js";
+} from "../../shared/util/validators";
 import { useForm } from "../../shared/hooks/form-hook";
 import "./PlaceForm.css";
 
