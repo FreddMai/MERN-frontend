33cdecc72b389295b59b753b6e3b508eeba19a8e
Freddy M <fredtein@gmail.com>
2020-07-03 15:34:59 +0300

    Signup using POST request to the backend

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -29,9 +29,31 @@ const Auth = () => {
     false
   );
 
-  const authSubmitHandler = (event) => {
+  const authSubmitHandler = async (event) => {
     event.preventDefault();
-    console.log(formState.inputs);
+
+    if (isLogin) {
+    } else {
+      try {
+        const resposne = await fetch("http://localhost:5000/api/users/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const responeData = await resposne.json();
+        console.log(responeData);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
     auth.login();
   };
 
